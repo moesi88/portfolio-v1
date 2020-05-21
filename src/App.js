2a9feb169a1e19442ad5af19c1d0302fbeb32542
Maher Kurdi <m.saadkrde@ltp.com>
2020-05-21 13:10:43 +0200

    portfolio first draft

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+   faUser
+} from '@fortawesome/free-solid-svg-icons';
+import {
+    // here must be the webs icons that needed to reach out to me like facebook github and so
+} from '@fortawesome/free-brands-svg-icons';
+
+//add imported icons to global library to make them available wherever the FontAwesomeIcon component is imported
+library.add(
+    faUser
+);
+
+
+function App() {
+  return (
+    <div className="App">
+        hiii am here
+    </div>
+  );
+}
+
+export default App;
