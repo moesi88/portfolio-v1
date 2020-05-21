f28441fb2ac24b53fe342d70ee2365feb887227a
Maher Kurdi <m.saadkrde@ltp.com>
2020-05-21 16:00:13 +0200

    portfolio first draft

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
-   faUser
+    faUser
 } from '@fortawesome/free-solid-svg-icons';
 import {
     // here must be the webs icons that needed to reach out to me like facebook github and so
 } from '@fortawesome/free-brands-svg-icons';
+import NavigationBar from "./components/NavigationBar/NavigationBar";
 
 //add imported icons to global library to make them available wherever the FontAwesomeIcon component is imported
 library.add(
@@ -16,11 +17,11 @@ library.add(
 
 
 function App() {
-  return (
-    <div className="App">
-        hiii am here
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavigationBar />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import style from './style/navigation_bar_wrapper.scss'
+
+
+class NavigationBar extends Component {
+    render() {
+        return (
+            <div className={style.navigation_bar_wrapper}>nav here</div>
+        );
+    }
+}
+
+export default NavigationBar;
